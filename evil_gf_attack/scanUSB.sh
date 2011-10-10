939d159bc2c2f24f4da0ddda59a720fb4fe518e5
Keith Lee <keith.lee2012@gmail.com>
2011-10-11 05:27:57 +0800

    Remove notify-send and change it to echo for status reporting

--- evil_gf_attack/scanUSB.sh
+++ evil_gf_attack/scanUSB.sh
@@ -14,7 +14,8 @@ do
 		mkdir -p /media/iPhone/var/root/Media/Cydia/AutoInstall
 		sleep 1
 		#nautilus $mntp
-		notify-send "iPhone Mounted" "The iPhone is now mounted"
+		echo "iPhone has been mounted"
+		#notify-send "iPhone Mounted" "The iPhone is now mounted"
 		deviceName=`ideviceinfo -s | grep -i "DeviceName" | awk '{print $2}'`	
 		newdstLocation=$dstLocation$deviceName
 		if [ ! -d $newdstLocation ]; then
@@ -30,15 +31,18 @@ do
 			ideviceinstaller -l > $newdstLocation/installedSoftware.txt
 			ideviceinfo -s > $newdstLocation/deviceInfo.txt
 			fusermount -u $mntp
-			notify-send "Backup Completed" "Backup Completed"
+			echo "Backup Completed"
+			#notify-send "Backup Completed" "Backup Completed"
 		else
-			notify-send "Skipping" "Device was already backed up"	
+			echo "Skipping. Device was already backed up"
+			#notify-send "Skipping" "Device was already backed up"	
 			sleep 10
 		fi
 			#break
 	else
 		fusermount -u $mntp
-		notify-send "iPhone Dismounted" "The iPhone is now dismounted"
+		echo "iPhone is not dismounted"
+		#notify-send "iPhone Dismounted" "The iPhone is now dismounted"
 	fi
 	sleep 5
 done
